4b627d04351ce257d573468e4e2fe4d42f23ead7
Olimpia Spyra <olimpia.spyra@gmail.com>
2021-02-03 18:35:21 +0100

    Add computerMove

--- js/functions.js
+++ js/functions.js
@@ -8,3 +8,6 @@ function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
 
+let computerMove = 'kamień';
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
