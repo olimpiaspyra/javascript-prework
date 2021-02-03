f52c3138b7cc8a2eae2cccb47523f99c426763cb
Olimpia Spyra <olimpia.spyra@gmail.com>
2021-02-03 18:38:46 +0100

    Add playerMove

--- js/functions.js
+++ js/functions.js
@@ -11,3 +11,7 @@ function clearMessages(){
 let computerMove = 'kamień';
 
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+let playerMove = 'papier';
+
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to ' + computerMove + ', to przegrywasz!');
