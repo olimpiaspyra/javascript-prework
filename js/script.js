a9acc2aaec99e446e1ed966d33add8129355adfa
Olimpia Spyra <olimpia.spyra@gmail.com>
2021-02-06 16:48:15 +0100

    Fix function displayResult and And displayScore

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+let computerPoint = 0;
+let playerPoint = 0;
+
 function playGame (playerInput) {
 
 	clearMessages();
@@ -60,32 +63,76 @@ function playGame (playerInput) {
 	// 	printMessage ('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.')
 	// }	
 
+	// function displayResult (argComputerMove, argPlayerMove) {
+	// 	console.log ('moves:', argComputerMove, argPlayerMove);
+		
+	// 	printMessage ('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+	
+	// 	if (argComputerMove == 'kamień' && argPlayerMove == 'papier') {
+	// 	printMessage ('Ty wygrywasz!')
+	// 	return win;
+	// 	} else if (argComputerMove == 'nożyce' && argPlayerMove == 'papier') {
+	// 		printMessage ('Ty wygrywasz!')
+	// 		return win;
+	// 	} else if (argComputerMove == 'nożyce' && argPlayerMove == 'kamień') {
+	// 		printMessage ('Ty wygrywasz!');
+	// 	} else if (computerMove == playerMove) {
+	// 		printMessage ('Remis!');
+	// 	} else {
+	// 	printMessage('Tym razem przegrywasz :(')
+	// 	return losses;
+	// 	}
+	// }
+
+	// displayResult (computerMove, playerMove);
+
+	// function score (argPoint) {
+	// 	if (argPoint == 1) playerPoint = playerPoint+1;
+			
+	// 	 else if (argPoint == 0) computerPoint = computerPoint+1;		
+	// 	return playerPoint, computerPoint;
+	// }
+	// score (displayResult (computerMove, playerMove));
+	// document.getElementById ('result').innerHTML = 'Wygrane komputera ' + computerPoint + ', wygrane gracza ' + playerPoint;
+
 	function displayResult (argComputerMove, argPlayerMove) {
 		console.log ('moves:', argComputerMove, argPlayerMove);
 		
 		printMessage ('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
 	
-		if (argComputerMove == 'kamień' && argPlayerMove == 'papier') {
-		printMessage ('Ty wygrywasz!');
-		} else if (argComputerMove == 'nożyce' && argPlayerMove == 'papier') {
+		if (argComputerMove === 'kamień' && argPlayerMove === 'papier' ||
+		 	argComputerMove === 'nożyce' && argPlayerMove === 'kamień' ||
+		 	argComputerMove === 'papier' && argPlayerMove === 'nożyce') {
 			printMessage ('Ty wygrywasz!');
-		} else if (argComputerMove == 'nożyce' && argPlayerMove == 'kamień') {
-			printMessage ('Ty wygrywasz!');
-		} else if (computerMove == playerMove) {
+			return 1;
+		} else if (computerMove === playerMove) {
 			printMessage ('Remis!');
 		} else {
-		printMessage('Tym razem przegrywasz :(');
+			printMessage('Tym razem przegrywasz :(');
+			return 0;
 		}
 	}
 
-	displayResult (computerMove, playerMove);
+	function displayScore (argPoint) {
+		console.log (computerPoint, playerPoint)
+		
+		if (argPoint === 1) playerPoint = ++playerPoint;			
+		else if (argPoint === 0) computerPoint = ++computerPoint;		
+		return playerPoint, computerPoint;
+		
+	}
+
+	displayScore (displayResult (computerMove, playerMove));
+	document.getElementById ('result').innerHTML = 'Wygrane komputera ' + computerPoint + ', wygrane gracza ' + playerPoint;	
+
 }
 
-	document.getElementById ('play-rock').addEventListener('click', function() {
+	document.getElementById ('play-rock').addEventListener ('click', function () {
 		playGame (1)});
 
-	document.getElementById ('play-paper').addEventListener ('click', function() {
+	document.getElementById ('play-paper').addEventListener ('click', function () {
 		playGame (2)});
 
-	document.getElementById ('play-scissors').addEventListener ('click', function() {
+	document.getElementById ('play-scissors').addEventListener ('click', function () {
 		playGame (3)});
+	
